fd8fe30ed5cbdf4b303bee64ac14c150826a1d1c
Tom Hinton <tomjhinton@gmail.com>
2022-10-01 23:42:23 +0100

    test

--- src/shaders/liquid1Frag.glsl
+++ src/shaders/liquid1Frag.glsl
@@ -14,6 +14,15 @@ uniform float uValueD;
 varying vec2 vUv;
 varying float vTime;
 
+void coswarp(inout vec3 trip, float warpsScale ){
+
+  float t = vTime +length(vUv) + sin(vUv.x * 20.);
+
+  trip.xyz += warpsScale * .1 * cos(3. * trip.yzx + (t * .25));
+  trip.xyz += warpsScale * .05 * cos(11. * trip.yzx + (t * .25));
+  trip.xyz += warpsScale * .025 * cos(17. * trip.yzx + (t * .25));
+
+}
 
 
 
@@ -25,6 +34,7 @@ void main(){
 
   vec3 color = vec3(uv.x, uv.y, uValueA);
 
+  coswarp(color, uValueB * 3.);
 
 
 
--- src/shaders/liquid2Frag.glsl
+++ src/shaders/liquid2Frag.glsl
@@ -1,3 +1,4 @@
+
 const float PI = 3.1415926535897932384626433832795;
 const float TAU = PI * 2.;
 
@@ -13,6 +14,15 @@ uniform float uValueD;
 varying vec2 vUv;
 varying float vTime;
 
+void coswarp(inout vec3 trip, float warpsScale ){
+
+  float t = vTime +length(vUv) + sin(vUv.x * 20.);
+
+  trip.xyz += warpsScale * .1 * cos(3. * trip.yzx + (t * .25));
+  trip.xyz += warpsScale * .05 * cos(11. * trip.yzx + (t * .25));
+  trip.xyz += warpsScale * .025 * cos(17. * trip.yzx + (t * .25));
+
+}
 
 
 
@@ -20,7 +30,12 @@ void main(){
   float alpha = 1.;
   vec2 uv = (gl_FragCoord.xy - uResolution * .5) / uResolution.yy ;
   uv = vUv;
-  vec3 color = vec3(1., uValueA, 0.);
+
+
+  vec3 color = vec3(uv.x,  uValueA, uv.y -uValueB);
+
+  coswarp(color, uValueB * 3.);
+
 
 
  gl_FragColor = vec4(color, alpha)  ;
